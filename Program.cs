176bf8360cc5db5ae2e37e8bcbe3ad86b11fc79b
Rômulo Henrique <henriqueromulo1997@gmail.com>
2023-10-20 01:18:37 -0300

    Adicionando Código e README

--- Program.cs
+++ Program.cs
@@ -1,4 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
 
-// Upando para o Git
+class Program
+{
+    static void Main()
+    {
+        // Nome e Senha do Usuário 
+        string usuarioPadrao = "admin";
+        string senhaPadrao = "admin";
+
+        Console.WriteLine(" Digite o nome de usuário:");
+        string nomeUsuario = Console.ReadLine();
+
+        Console.WriteLine("\n Digite a senha:");
+        string senha = Console.ReadLine();
+
+        if (nomeUsuario == usuarioPadrao && senha == senhaPadrao)
+        {
+            // Sistema que Verifica a Hora
+            DateTime agora = DateTime.Now;
+            string saudacao = ObterSaudacao(agora);
+            Console.WriteLine($"{saudacao}, {nomeUsuario}!");
+        }
+        else
+        {
+            Console.WriteLine("\n Credenciais inválidas. Tente novamente. \n");
+        }
+    }
+
+    static string ObterSaudacao(DateTime agora)
+    {
+        // Sistema de Saudação de acordo com horário obtido no DataTime.Now
+        int hora = agora.Hour;
+        string saudacao;
+
+        if (hora >= 6 && hora <= 12)
+        {
+            saudacao = "\n Bom Dia";
+        }
+        else if (hora > 12 && hora <= 18)
+        {
+            saudacao = "\n Boa Tarde";
+        }
+        else if (hora > 18 && hora <= 24)
+        {
+            saudacao = "\n Boa Noite";
+        }
+        else
+        {
+            saudacao = "\n Boa Madrugada";
+        }
+
+        return saudacao;
+    }
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+# Calculadora-C-Sharp
+- Projeto realizado por Rômulo Henrique
+
+## Tecnologia utilizada
+- Visual Code
+- .net 6
+- C#
+- Git
+- Git Hub
+
+## Finalidade
+- Esse projeto marca o início de uma caminhada pela Linguagem C#, um desafio criado por Oswaldo Oliveira para me ajudar dar os primeiros passos na carreira de Dev
